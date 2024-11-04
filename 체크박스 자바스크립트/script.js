2794bbe0c1e7539a36713e2adc47ce2ed461728b
kimmoochan <kimfkgml77@gmail.com>
2024-11-04 12:27:26 +0900

    자바스크립트 todolist

--- 체크박스 자바스크립트/script.js
+++ 체크박스 자바스크립트/script.js
@@ -1,41 +1,45 @@
-// const checkbox= document.createElement("input");
-//         const innertext=document.getElementById('save').value;
-//         const crontext=document.getElementById('newtext').innerText;
-//         const container =document.getElementById("container");
-const btnEl=document.querySelector('#savebutton');
-function btn(){
-    // const list = document.createElement("li");
-    const checkbox= document.createElement("input");
-    // const innertext=document.getElementById('save').value;
-    // const crontext=document.getElementById('newtext').innerText=innertext;
-    // const container =document.getElementById("container");
-    // 1. 추가할 값을 input창에서 읽어온다
-    const save 
-    = document.getElementById('save').value;
-    
-    // 2. 추가할 li element 생성
-    // 2-1. 추가할 li element 생성
-    const li = document.createElement("li");
-    
-    // 2-2. li에 id 속성 추가 
-    li.setAttribute('id',save);
-    
-    // 2-3. li에 text node 추가 
-    const textNode = document.createTextNode(save);
-    li.appendChild(textNode);
-    
-    // 3. 생성된 li를 ul에 추가
-    document
-    .getElementById('fruits')
-    .appendChild(li);
-    
-    container.appendChild(checkbox);
-    checkbox.type = 'checkbox';
 
-}
-    
-  btnEl.addEventListener('click',()=>{
-    btn();
-  })
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const toDo = document.querySelector('#save')
+    const addButton = document.querySelector('#savebutton')
+    const toDoList = document.querySelector('#main')
+
+    addButton.addEventListener('click', (event) => {
+        const item = document.createElement('div') 
+
+        const checkBox = document.createElement('input')
+        checkBox.setAttribute('type','checkbox')
+
+
+        const text = document.createElement('span')
+        text.textContent = toDo.value
+
+        const removeButton = document.createElement('button') 
+        removeButton.textContent = '삭제'
+
+
+        removeButton.addEventListener('click', (event) => {
+            event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode)
+})
+
+        checkBox.addEventListener('change', (event) => {
+            if(checkBox.checked){
+                text.style.textDecorationLine = "opaci"
+            }else{
+                text.style.textDecorationLine = "none"
+            }
+        })
+                
+        item.appendChild(checkBox)
+        item.appendChild(text)
+        item.appendChild(removeButton)
+
+        toDoList.appendChild(item)
+        toDo.value = ''
+        })
+
+
+})
 
     
