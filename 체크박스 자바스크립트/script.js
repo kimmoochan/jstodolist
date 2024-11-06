8fb6ff0ccdeb39dc34c94d1e2d36e0c8cf2c83ae
kimmoochan <kimfkgml77@gmail.com>
2024-11-06 19:11:09 +0900

    자바스크립트

--- 체크박스 자바스크립트/script.js
+++ 체크박스 자바스크립트/script.js
@@ -1,28 +1,30 @@
 
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    const toDo = document.querySelector('#save')
-    const addButton = document.querySelector('#savebutton')
-    const toDoList = document.querySelector('#main')
+const toDo = document.querySelector('#save')
+const Button = document.querySelector('#savebutton')
+const main = document.querySelector('#main')
 
-    addButton.addEventListener('click', (event) => {
+  document.addEventListener('DOMContentLoaded', () => {
+    
+    
+    Button.addEventListener('click', (event) => {
         const item = document.createElement('div') 
 
         const checkBox = document.createElement('input')
         checkBox.setAttribute('type','checkbox')
-
-
+        
+        
         const text = document.createElement('span')
         text.textContent = toDo.value
-
+        
         const removeButton = document.createElement('button') 
         removeButton.textContent = '삭제'
-
-
+        
+        
         removeButton.addEventListener('click', (event) => {
             event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode)
-})
-
+            
+        })
+        
         checkBox.addEventListener('change', (event) => {
             if(checkBox.checked){
                 text.style.opacity = "0.5";
@@ -30,16 +32,55 @@
                 text.style.opacity = "1"
             }
         })
-                
+        
         item.appendChild(checkBox)
         item.appendChild(text)
         item.appendChild(removeButton)
-
-        toDoList.appendChild(item)
+        
+        main.appendChild(item)
         toDo.value = ''
-        })
-
-
+    })
+    
+    
+    
 })
+
+function keyCodeCheck () {
+    
+    if(window.event.keyCode === 13){
+        const item = document.createElement('div') 
+
+        const checkBox = document.createElement('input')
+        checkBox.setAttribute('type','checkbox')
+        
+        
+        const text = document.createElement('span')
+        text.textContent = toDo.value
+        
+        const removeButton = document.createElement('button') 
+        removeButton.textContent = '삭제'
+        
+        
+        removeButton.addEventListener('click', (event) => {
+            event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode)
+            
+        })
+        
+        checkBox.addEventListener('change', (event) => {
+            if(checkBox.checked){
+                text.style.opacity = "0.5";
+            }else{
+                text.style.opacity = "1"
+            }
+        })
+        
+        item.appendChild(checkBox)
+        item.appendChild(text)
+        item.appendChild(removeButton)
+        
+        main.appendChild(item)
+        toDo.value = ''
+    }
+}
 
     
