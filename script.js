9995f93be15abcd5d23202e606818dba97e80991
kimmoochan <kimfkgml77@gmail.com>
2025-01-23 20:23:43 +0900

    todolist 완성

--- script.js
+++ script.js
@@ -17,9 +17,11 @@ const main = document.querySelector('#main')
             // input 생성후 type을 checkbox로
             const checkBox = document.createElement('input')
             checkBox.setAttribute('type','checkbox')
+            checkBox.setAttribute("class", "Allcheckbox")
             
             // span 생성후 span 에다가 input value를 넣어준다.
             const text = document.createElement('span')
+            text.setAttribute("class", "AllSpan")
             text.textContent = toDo.value
             
             // 버튼 생성후 버튼 텍스트를 '삭제'로 하고 오른쪽정렬
@@ -65,18 +67,21 @@ const main = document.querySelector('#main')
             }else{
 
             const item = document.createElement('div') 
+            item.style.marginBottom = "20px"
             
             const checkBox = document.createElement('input')
             checkBox.setAttribute('type','checkbox')
+            checkBox.setAttribute("class", "Allcheckbox")
             
             
             const text = document.createElement('span')
+            text.setAttribute("class", "AllSpan")
             text.textContent = toDo.value
             
             const removeButton = document.createElement('button') 
             removeButton.textContent = '삭제'
             removeButton.style.float = 'right';
-            
+
             
             removeButton.addEventListener('click', (event) => {
                 event.currentTarget.parentNode.parentNode.removeChild(event.currentTarget.parentNode)
@@ -86,8 +91,10 @@ const main = document.querySelector('#main')
             checkBox.addEventListener('change', (event) => {
                 if(checkBox.checked){
                     text.style.opacity = "0.5";
+                    text.style.textDecoration = "line-through";
                 }else{
                     text.style.opacity = "1"
+                    text.style.textDecoration = "none";
                 }
                 
         })
@@ -104,3 +111,58 @@ const main = document.querySelector('#main')
 }
 
 
+const AllCheck = document.querySelector('#AllCheck');
+
+AllCheck.addEventListener('click', function(){
+
+    const isChecked = AllCheck.checked;
+
+    if(isChecked){
+        const checkboxes = document.querySelectorAll('.Allcheckbox');
+
+        for(const checkbox of checkboxes){
+            checkbox.checked = true;
+            const texts = document.querySelectorAll('.AllSpan')
+        texts.forEach(function(text) {
+            text.style.opacity = "0.5";
+            text.style.textDecoration = "line-through";
+        });
+        }
+    }
+
+    else{
+        const checkboxes = document.querySelectorAll('.Allcheckbox');
+        for(const checkbox of checkboxes){
+            checkbox.checked = false;
+            const texts = document.querySelectorAll('.AllSpan')
+        texts.forEach(function(text) {
+            text.style.opacity = "1";
+            text.style.textDecoration = "none";
+        });
+            
+        }
+    }
+    
+    
+});
+
+
+const checkboxes = document.querySelectorAll('.Allcheckbox');
+for(const checkbox of checkboxes){
+  checkbox.addEventListener('click',function(){
+    
+    const totalCnt = checkboxes.length;
+  
+    const checkedCnt = document.querySelectorAll('.Allcheckbox:checked').length;
+    
+    if(totalCnt == checkedCnt){
+      document.querySelector('#AllCheck').checked = true;
+    }
+    else{
+      document.querySelector('#AllCheck').checked = false;
+    }
+    
+  });
+  
+  
+}
